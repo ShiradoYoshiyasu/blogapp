0575759e5d1caa68efe63deb3f64b555bbf10efd
Yoshiyasu Shirado <y-shirado@towatech.net>
2016-03-07 09:35:38 +0900

    kadai1-1 新規記事のタイトルをデーターベースに保存する機能を追加

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,4 +12,17 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def create
+    p "test"
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to articles_path
+  end
+
+  private
+
+  def article_params
+    params[:article].permit(:title)
+  end
+
 end
